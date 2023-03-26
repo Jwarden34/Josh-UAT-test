2cd383f68472ce5d0760d00d362aa81903b92a8c
Jwarden34 <127161530+Jwarden34@users.noreply.github.com>
2023-03-26 17:38:19 -0400

    Add files via upload

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ var lastname = document.getElementById("last");
 
 if(password.value.length ==3 && firstname.value.length==10 && lastname.value.length>1 || lastname.value.length==10){
    alert("Login good!");
-   window.location.replace("UAThtml.html");
+   window.location.replace("index.html");
 return false;
 
 }
